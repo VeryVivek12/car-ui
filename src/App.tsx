f6bb3ae364857e7a8c2344aaa30a429b36884274
Vivek Topiya <vivektopiya12@gmail.com>
2024-10-30 10:15:54 +0530

    notification sonner removed - updated api call endpoints - publish frequency updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import React, {useState, useEffect} from 'react'
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select"
+import {useEffect, useState} from 'react'
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue,} from "@/components/ui/select"
 import {Input} from "@/components/ui/input"
 import {Button} from "@/components/ui/button"
-import {Toaster} from "@/components/ui/sonner"
-import {toast} from "sonner"
 
 import './App.css'
 
@@ -19,35 +11,15 @@ function App() {
     const [users, setUsers] = useState<any>([]);
     const [currentVehicle, setCurrentVehicle] = useState()
     const [currentUser, setCurrentUser] = useState("")
-    const [currentMileage, setCurrentMileage] = useState(5)
-
-
-    useEffect(() => {
-        const fetchNotifications = async () => {
-            try {
-                const response = await fetch(`http://localhost:8080/api/v1/notification?vehicleId=${currentVehicle}&userId=${currentUser}`);
-                const data = await response.json();
-                toast(data.notificationMessage)
-            } catch (error) {
-                console.error('Fetch error:', error);
-            }
-        };
-
-        // Fetch notifications immediately on mount
-        fetchNotifications();
-
-        // Set up an interval to fetch notifications every 5 seconds
-        const intervalId = setInterval(fetchNotifications, 5000);
-
-        // Clean up the interval on component unmount
-        return () => clearInterval(intervalId);
-    }, [currentVehicle, currentUser]);
+    const [currentUsersEfficiencyTarget, setCurrentUsersEfficiencyTarget] = useState()
+    const [currentMileage, setCurrentMileage] = useState(14)
+    const [notificationText, setNotificationText] = useState("")
 
     useEffect(() => {
-        // Fetch vehicles and users when the component mounts
+        // Fetch vehicles when the component mounts
         const fetchVehiclesAndUsers = async () => {
             try {
-                const vehiclesResponse = await fetch('http://localhost:8080/api/v1/vehicles/all');
+                const vehiclesResponse = await fetch('http://localhost:8080/api/v1/vehicles');
                 const vehiclesData = await vehiclesResponse.json();
                 setVehicles(vehiclesData);
 
@@ -61,7 +33,7 @@ function App() {
 
     async function fetchUsersForVehicle(vehicleId: string) {
         try {
-            const usersResponse = await fetch(`http://localhost:8080/api/v1/users/test?vehicleId=${vehicleId}`);
+            const usersResponse = await fetch(`http://localhost:8080/api/v1/users/vehicle/${vehicleId}`);
             const usersData = await usersResponse.json();
             setUsers(usersData);
             setCurrentVehicle(vehicleId)
@@ -71,19 +43,35 @@ function App() {
         }
     }
 
+    async function fetchEfficiencyTargetForCurrentUser(userId: string) {
+        try {
+            const usersResponse = await fetch(`http://localhost:8080/api/v1/efficiency-targets/user/${userId}/vehicle/${currentVehicle}`);
+            const targetData = await usersResponse.json();
+            setCurrentUsersEfficiencyTarget(targetData.efficientTargetValue)
+            setCurrentUser(userId)
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
+
+
     async function publishMileage() {
         for (let step = 0; step < 5; step++) {
-            await fetch(`http://localhost:8080/api/v1/mileage`, {
-                method: "POST",
-                body: JSON.stringify({
-                    vehicleId: currentVehicle,
-                    userId: currentUser,
-                    currentMileage: currentVehicle
-                }),
-                headers: {
-                    "Content-Type": "application/json",
-                }
-            });
+            setTimeout(async () => {
+                const res = await fetch(`http://localhost:8080/api/v1/mileage`, {
+                    method: "POST",
+                    body: JSON.stringify({
+                        vehicleId: currentVehicle,
+                        userId: currentUser,
+                        currentMileage: currentMileage
+                    }),
+                    headers: {
+                        "Content-Type": "application/json",
+                    }
+                });
+                const data = await res.json();
+                setNotificationText(data.message);
+            }, step * 1000);
         }
     }
 
@@ -102,7 +90,8 @@ function App() {
                         </SelectTrigger>
                         <SelectContent>
                             {vehicles.map((vehicle) => (
-                                <SelectItem key={vehicle.id} value={vehicle.id}>{vehicle.name}</SelectItem>
+                                <SelectItem key={vehicle.vehicleId}
+                                            value={vehicle.vehicleId}>{vehicle.model}</SelectItem>
                             ))}
                         </SelectContent>
                     </Select>
@@ -110,30 +99,39 @@ function App() {
                 {/* User Selection*/}
                 <div className="flex flex-row mx-2 items-center">
                     <div className="text-lg mx-2 w-[180px]">Select User :</div>
-                    <Select onValueChange={value => setCurrentUser(value)}>
+                    <Select onValueChange={value => fetchEfficiencyTargetForCurrentUser(value)}>
                         <SelectTrigger className="w-[180px]">
                             <SelectValue placeholder="User"/>
                         </SelectTrigger>
                         <SelectContent>
                             {users.map((user) => (
-                                <SelectItem key={user.id} value={user.id}>{user.name}</SelectItem>
+                                <SelectItem key={user.userId} value={user.userId}>{user.name}</SelectItem>
                             ))}
                         </SelectContent>
                     </Select>
                 </div>
 
+                {
+                    currentUsersEfficiencyTarget &&
+                    <p>Efficiency Target: {currentUsersEfficiencyTarget}</p>
+                }
+
                 <hr/>
                 {/* Mileage publisher*/}
                 <div className="flex flex-row m-2 items-center ">
                     <div className="text-lg mx-2 w-[180px]">Mileage value to publish :</div>
-                    <Input className="w-[180px]" type="number" value={currentMileage} min="0"
-                           max="10" step="0.1"
+                    <Input className="w-[180px]" type="number" value={currentMileage} min="14"
+                           max="30" step="1"
                            onChange={event => setCurrentMileage(event.target.value)}/>
                 </div>
                 <Button variant="default" className="m-2 w-[180px]" onClick={publishMileage}>Publish Mileage</Button>
             </div>
             <hr/>
-            <Toaster closeButton={true} position="top-right"/>
+
+            <div className="flex flex-col">
+                <h2 className="text-xl">Notification</h2>
+                <div className="border-2 p-2 w-fit">{notificationText}</div>
+            </div>
         </main>
     )
 }
